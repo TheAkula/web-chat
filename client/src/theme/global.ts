f54ede67555371675ee297e16068b36bd246ddaf
nikolay <gugalohikolaychur@gmail.com>
2022-06-23 15:15:50 +0300

    add fonts

--- client/src/theme/global.ts
+++ client/src/theme/global.ts
@@ -1,10 +1,26 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: OpenSans;
+    src: url('./fonts/OpenSans-Regular.ttf');
+    font-weight: 400;
+  }
+  @font-face {
+    font-family: OpenSans;
+    src: url('./fonts/OpenSans-Bold.ttf');
+    font-weight: 700;
+  }
+  @font-face {
+    font-family: OpenSans;
+    src: url('./fonts/OpenSans-Medium.ttf');
+    font-weight: 500;
+  }
+  
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: sans-serif;
+    font-family: OpenSans, sans-serif;
   }
 `;
